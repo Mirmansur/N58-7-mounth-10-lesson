5b7103a8b634899f18279e5136ef2625df150ab8
Mirmansur <147687431+Mirmansur@users.noreply.github.com>
2024-08-03 21:38:54 +0500

    last

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Input, Button } from "antd";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = (values) => {
+    setLoading(true);
+    const { username, password } = values;
+    if (username === "john32" && password === "12345678") {
+      toast.success("Login successful!");
+      navigate("/dashboard");
+    } else {
+      toast.error("Invalid username or password");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <Form name="login" onFinish={handleLogin} className="form">
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: "Please input your Username!" }]}
+        >
+          <Input placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: "Please input your Password!" }]}
+        >
+          <Input.Password placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" loading={loading}>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
